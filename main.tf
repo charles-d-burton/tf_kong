bd9e7cc32016db51e132e5d24ff35f97931a0d33
charles-d-burton <charles.burton@mindflash.com>
2017-12-14 16:47:55 -0700

    updated target type

--- main.tf
+++ main.tf
@@ -113,10 +113,11 @@ resource "aws_security_group_rule" "internal_allow_admin_secure" {
 
 #Setup target groups
 resource "aws_alb_target_group" "external_http_target_group" {
-  name     = "${var.tag_name}-alb-http"
-  port     = 8000
-  protocol = "HTTP"
-  vpc_id   = "${var.vpc_id}"
+  name        = "${var.tag_name}-alb-http"
+  port        = 8000
+  protocol    = "HTTP"
+  vpc_id      = "${var.vpc_id}"
+  target_type = "ip"
 
   health_check {
     path    = "/status"
